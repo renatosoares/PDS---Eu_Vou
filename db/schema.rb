205df49c8007f4197b7cd0ddfbae5a1bcf8d3490
Marcelo Odir <marceloodir@gmail.com>
2015-07-01 11:24:57 -0300

    Branch aberto para teste de implementação de autenticação com a Gem Devise: - Instalação da Gem Devise; - Criação do Modelo Users com Devise - Inclusão dos campos nome, sexo e foto nas permissões de recebimento - aplicação de filtros no controller events - pequenas modificações nas view para inclusão do usuário conectado e links de sign_out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619190413) do
+ActiveRecord::Schema.define(version: 20150701113414) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "titulo"
@@ -36,5 +36,35 @@ ActiveRecord::Schema.define(version: 20150619190413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "nome",                                   null: false
+    t.string   "foto"
+    t.string   "sexo"
+    t.integer  "pontuacao",              default: 0,     null: false
+    t.boolean  "admin",                  default: false, null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,     null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
