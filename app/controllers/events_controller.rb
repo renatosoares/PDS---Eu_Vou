fa5316ba8bef8468f7f919c2d203248d7ed2c5dd
Marcelo Odir <marceloodir@gmail.com>
2015-07-13 12:18:31 -0300

    Finalizado o relacionamentos entre User e Event: -  Criado o controlador Euvou e método GET confirmation que será responsável por incluir o usuário corrente (current_user) na lista de usuários que confirmaram presente em um determinado evento (event.users << current_user). Com validação de um clique por usuário/evento; - Alteração da view de eventos (root_path) para incluir o botão de EuVou!, que estará habilitado apenas uma vez por usuário/evento; - Alteração da view de eventos (root_path) para incluir a quantidade de usuários que clicaram em eu vou;

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only:  [:edit, :update, :destroy, :create]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   # GET /events
   # GET /events.json
